c573dee640d37975b2c2e2d5d415ebdbea3b61d9
Rehaan-Kumar <73431008+Rehaan-Kumar@users.noreply.github.com>
2021-04-28 22:54:36 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+var player_1_name = localStorage.getItem("player_1");
+var player_2_name = localStorage.getItem("player_2");
+document.getElementById("player1_name").innerHTML=player_1_name + ": ";
+document.getElementById("player2_name").innerHTML=player_2_name + ": ";
+var player_1_score = 0;
+var player_2_score = 0;
+document.getElementById("player1_score").innerHTML=player_1_score;
+document.getElementById("player2_score").innerHTML=player_2_score;
+document.getElementById("player_question").innerHTML="Question Turn: " + player_1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn: " + player_2_name;
